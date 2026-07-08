137cb96c06029c4d4055e113ff3d6cfbf6479d77
Pyraxxz <sugrutaimako@gmail.com>
2026-07-08 12:10:06 +0000

    fix: add missing services.dart import for HapticFeedback

--- lib/screens/marketplace/business_profile_screen.dart
+++ lib/screens/marketplace/business_profile_screen.dart
@@ -26,6 +26,7 @@ import 'dart:convert';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:go_router/go_router.dart';
