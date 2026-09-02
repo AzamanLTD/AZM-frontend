dfa8d2a5ec73f8d5adfad1e08a0672059709d610
Taimako Shamsudeen Abdul-Mumin Sugru <57481825+pyraxxz@users.noreply.github.com>
2026-09-02 09:26:31 +0000

    test: widen dining tempo window

--- test/widgets/marketplace/restaurant_commit_surface_test.dart
+++ test/widgets/marketplace/restaurant_commit_surface_test.dart
@@ -175,7 +175,7 @@ void main() {
     );
 
     await tester.tap(find.text('Add'));
-    await tester.pump(const Duration(milliseconds: 250));
+    await tester.pump(const Duration(milliseconds: 320));
     expect(quickCommitted, isTrue);
 
     var relaxedCommitted = false;
@@ -229,7 +229,7 @@ void main() {
     await tester.pump();
 
     await tester.tap(find.text('Add'));
-    await tester.pump(const Duration(milliseconds: 250));
+    await tester.pump(const Duration(milliseconds: 320));
     expect(committed, isTrue);
   });
 }
